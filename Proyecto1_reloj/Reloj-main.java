57ab71301ea3ab640d7d2e400e446fe48218eb05
ricardo-tapia <jose.ricardo.dorado.tapia.1506@gmail.com>
2020-02-18 00:34:47 -0600

    Proyecto reloj

---
+++ Proyecto1_reloj/Reloj-main.java
@@ -0,0 +1,15 @@
+package ProyectoReloj;
+
+import ico.fes.poo.Reloj; 
+
+public class Proyecto2 {
+
+    
+
+    public static void main(String[] args) {       
+        
+        Reloj prueba = new Reloj(22,47,1);
+        prueba.mostrarhora();
+                
+    }  
+}
